1433bb1f6c314bb2405c25c0707e5139c20be4d4
Esa Koskinen <esakoskin2@gmail.com>
2026-06-10 01:24:44 +0900

    feat: add service worker to prevent images going stale in airplane mode

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,15 @@ import { renderDraft, randomDraftOptions, emptyRosterChoices } from './render'
 import { setAutoFight } from './biomes'
 import { startRun, debugWinBattle, debugAddGeosphere } from './game'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then(() => console.log('Airplane-mode cache activated!'))
+      .catch((err) => console.error('Cache registration failed:', err))
+  })
+}
+
 $('menuRandomBtn').onclick = startRandomGame
 $('menuDraftBtn').onclick = startDraftGame
 $('menuHelpBtn').onclick = showHelpRules
